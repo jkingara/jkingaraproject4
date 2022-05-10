41c08b5db7f2b5ed329ec8b06c30cd691d60e9c3
KING.JOSEPH <jkingara@student.bridgew.edu>
2022-05-10 13:37:31 -0400

    commited project

---
+++ main.py
@@ -0,0 +1,22 @@
+
+import openpyxl
+import openpyxl.utils
+
+def main():
+
+    open_worksheet = openpyxl.load_workbook("countypopchange2020")
+    get_losses = open_worksheet.active
+
+#function 2
+def worksheet():
+    sheet = openpyxl.load_workbook("countypopchange2020")
+    data = sheet.active
+    return data
+
+#funtion 3
+def losses():
+
+#functin 4
+def process():
+
+
